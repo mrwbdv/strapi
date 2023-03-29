f24d74f8058f199501649ba3dafab2d3814f2b66
akmaral.s <akmaral.serikova@tarlanpayments.kz>
2023-03-29 19:05:45 +0600

    feat: add config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,14 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      header: "*",
+      origin: "https://appsecfest.kz/",
+      credentials: false
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
